a1a26faf5ba7b1d0ee8858d8203d05c938683fc1
Zhiyue Huang <zhiyuehuang@yeah.net>
2021-06-27 23:14:58 +0800

    Potato Animation & UI codes

--- Potato Farm/Assets/Scripts/GrowingPotato.cs
+++ Potato Farm/Assets/Scripts/GrowingPotato.cs
@@ -8,15 +8,31 @@ public class GrowingPotato : MonoBehaviour
 
     public Image[] slots;
 
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject canvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 999f, layerMask);
+
+            if (hit)
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    canvas.SetActive(true);
+                }
+            }
+        }
     }
 }
